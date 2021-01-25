4c16b7ace0dff406213a62b4d80372b8731ac8c4
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-01-25 10:05:30 -0500

    #3 Prototipado de variable de datos

---
+++ app2.ts
@@ -0,0 +1,25 @@
+(function(){
+    let mensajes : string = 'Hola mundo !!';
+    let  numero : number = 123;
+    let booleano : boolean = true;
+    let tomorrow: Date = new Date();
+// any  soporta cualquier tipo de variable
+    let anymore;
+    anymore = mensajes;
+    anymore = numero;
+    anymore = booleano;
+    anymore = tomorrow;
+
+    let person = {
+        first_bane : 'Tomas' ,
+        years : 35
+    };
+
+    person = {
+        first_bane : 'Jhon' ,
+        years : 40 ,
+    //    occupation: 'miner'
+    }
+
+
+})();
