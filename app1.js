717f07c3465e5ca8463d8e0b8741a174931c10a7
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-01-27 09:35:39 -0500

    #8 Promesas dentro de funciones con timeout

--- app1.js
+++ app1.js
@@ -1,10 +1,10 @@
 "use strict";
 (function () {
-    var mensajes = 'Hola mundo !!';
+    let mensajes = 'Hola mundo !!';
     var mensaje2 = 'Soy el presidente';
-    var URL_DESCRIPT = 'Me llamo Donald';
+    const URL_DESCRIPT = 'Me llamo Donald';
     if (true) {
-        var mensajes_1 = 'Como estan ?';
+        let mensajes = 'Como estan ?';
         mensaje2 = 'Obama es el presidente de USA';
     }
     console.log(mensajes, mensaje2);
