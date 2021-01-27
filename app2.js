7a0dc5466222e5cdab1f650a2b3774c50e915ac0
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-01-27 11:06:36 -0500

    #10 Menejo de interfaces dentro de funciones

--- app2.js
+++ app2.js
@@ -1,3 +1,18 @@
 "use strict";
-(function () {
+(() => {
+    //                                 {nombre: string}
+    const enviarTrabajador = (persona) => {
+        console.log(`El trabajador ${persona.nombre} ya esta presente. `);
+    };
+    const finTrabajador = (persona) => {
+        console.log(`El trabajador ${persona.nombre} se ha retirado. `);
+    };
+    /// poner reglas que cumplan con los campos declarados en la interface
+    const employee = {
+        nombre: 'Jerson',
+        //   apellido: 'Monterrey' ,
+        edad: 35
+    };
+    enviarTrabajador(employee);
+    finTrabajador(employee);
 })();
